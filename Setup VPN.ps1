67967550307e3cb2b2dc8081447a68c0001f29a3
Jonny Elrod <33322644+DukeSilver1357@users.noreply.github.com>
2020-07-07 16:14:54 -0400

    Add files via upload

---
+++ Setup VPN.ps1
@@ -0,0 +1,17 @@
+
+
+function Add-Remotesetup {
+	Clear-Host
+	Write-Host "================ Remote User Setup Tool ================"
+	[string]$Name = Read-Host -Prompt 'VPN Name'
+	    
+	[string]$RouterAddress =  Read-Host -Prompt 'Public Address'
+
+	[string]$PreSharedKey =  Read-Host -Prompt 'PSK'
+
+	#Creates a Global VPN Connection with MSChap v2 enabled.
+	Add-VpnConnection -RememberCredential -Name "$Name" -ServerAddress "$RouterAddress" -TunnelType L2tp -EncryptionLevel Optional -AllUserConnection -AuthenticationMethod MSChapv2 -L2tpPsk "$PreSharedKey" -Force
+	REG ADD HKLM\SYSTEM\CurrentControlSet\Services\PolicyAgent /v AssumeUDPEncapsulationContextOnSendRule /t REG_DWORD /d 0x2 /f
+}
+
+Add-Remotesetup
